119f0e4866734bd6e2b9052129cdec7120bf6479
rickoneeleven <pinescore@outlook.com>
2020-09-06 14:02:35 +0100

    does it work?

--- app/Http/Controllers/PartyTest.php
+++ app/Http/Controllers/PartyTest.php
@@ -9,7 +9,15 @@ class PartyTest extends Controller
 {
     public function index() {
         //dd(GitHub::repo()->show('rickoneeleven', 'pinescore_engine'));
-        dd(GitHub::user()->show('rickoneeleven'));
+
+        //dd(GitHub::user()->events('rickoneeleven'));
+        echo GitHub::user()->events('rickoneeleven')[0]["created_at"];
+        
+        
+        //$json = file_get_contents("https://api.github.com/users/rickoneeleven/events");
+        //$data = json_decode($json, true);
+        //dd($data);
+
         //$client->api('user')->show('KnpLabs');
     }
     
