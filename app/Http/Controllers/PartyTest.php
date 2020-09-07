bdf6dfaeb8543fb704e3c6c06883721d1375f300
rickoneeleven <pinescore@outlook.com>
2020-09-07 19:57:47 +0100

    changed it so green means go only if commit is today, not based on x hours

--- app/Http/Controllers/PartyTest.php
+++ app/Http/Controllers/PartyTest.php
@@ -14,16 +14,19 @@ class PartyTest extends Controller
         $lights = $client->getLights();
         $light = $lights[4]; //stripper
 
-        if (strtotime($lastCommitTime) <= strtotime('-36 hours')) {
-            if($light->isOn()) { //no commit last 24 hours
+        //if (strtotime($lastCommitTime) <= strtotime('-12 hours')) {
+        if(date('Ymd') != date('Ymd', strtotime($lastCommitTime))) {
+            if($light->isOn()) { //no commit today
                 $light->setHue("4541 4");
                 $light->setBrightness(180); 
             }
-        } else { //commit in last 24 hours
+        } else { //commit today
             $light->setOn(True);
             $light->setBrightness(180);
             $light->setRGB(138, 245, 66); //green
         }
     }
+
+    
     
 }
