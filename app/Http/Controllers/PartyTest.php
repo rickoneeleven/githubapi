15f0a5b45a58fc62fe582b041b6ef32f60c6fd38
rickoneeleven <pinescore@outlook.com>
2020-09-07 18:54:09 +0100

    it's done baby, it's a wrap. light goes green if we have a commit in last 36 hours

--- app/Http/Controllers/PartyTest.php
+++ app/Http/Controllers/PartyTest.php
@@ -8,17 +8,22 @@ use GrahamCampbell\GitHub\Facades\GitHub;
 class PartyTest extends Controller
 {
     public function index() {
-        //dd(GitHub::repo()->show('rickoneeleven', 'pinescore_engine'));
+        $lastCommitTime =  GitHub::user()->events('rickoneeleven')[0]["created_at"];
 
-        //dd(GitHub::user()->events('rickoneeleven'));
-        echo GitHub::user()->events('rickoneeleven')[0]["created_at"];
-        
-        
-        //$json = file_get_contents("https://api.github.com/users/rickoneeleven/events");
-        //$data = json_decode($json, true);
-        //dd($data);
+        $client = new \Phue\Client('192.168.1.43', env("PHILIPS_BRIDGE_USERNAME"));
+        $lights = $client->getLights();
+        $light = $lights[4]; //stripper
 
-        //$client->api('user')->show('KnpLabs');
+        if (strtotime($lastCommitTime) <= strtotime('-36 hours')) {
+            if($light->isOn()) { //no commit last 24 hours
+                $light->setHue("4541 4");
+                $light->setBrightness(180); 
+            }
+        } else { //commit in last 24 hours
+            $light->setOn(True);
+            $light->setBrightness(180);
+            $light->setRGB(138, 245, 66); //green
+        }
     }
     
 }
